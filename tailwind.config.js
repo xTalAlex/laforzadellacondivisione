8bf72e0d310ff0899cae42fa268118f8298d8b4a
xTalAlex <talale93@hotmail.it>
2022-06-24 17:24:18 +0200

    Homepage Layout + colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,17 @@ module.exports = {
         extend: {
             
             colors: {
+                "green-haze-600": "#00BD59",
+                "green-haze-700": "#009446",
+                "green-haze-800": "#016A33",
 
+                "congress-blue-700": "#0051AC",
+                "congress-blue-800": "#003E83",
+                "congress-blue-900": "#012B59",
+                
+                "supernova-400": "#FFD529",
+                "supernova-500": "#FFCD00",
+                "supernova-600": "#D4AB02",
             },
 
         },
